1e7b6f2dd7c3f30374f8a5ee7db2d84fcc54e13b
cybrsucks <cybrs36@gmail.com>
2022-05-18 16:19:15 +0800

    testing commit 1

--- src/main/java/org/study/work/WorkApplication.java
+++ src/main/java/org/study/work/WorkApplication.java
@@ -2,12 +2,15 @@ package org.study.work;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class WorkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
